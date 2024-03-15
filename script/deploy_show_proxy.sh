65ab00babe9caf3ea095bda2d18f2a8afc1d127d
taayyohh <milesxbridges@gmail.com>
2024-03-14 22:46:29 -0400

    update abis

--- script/deploy_show_proxy.sh
+++ script/deploy_show_proxy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export ETH_RPC_URL="https://sepolia.infura.io/v3/2WzgmaEOqz7wdfQaPYe2hnAFSKg"
-export PRIVATE_KEY="9ad8ea34cdd8e1f6f4272c7884a62da1c7db612377999eab3d8cf9a653e4c19a"
-export ETHERSCAN_API_KEY="IPUKZ3RRCUJ47Q95JKIBW58Z3EFCDVAMX6"
-export SELLOUT_PROTOCOL_WALLET="0x1dD37D479ac16113fF8f160210Ee209944d2b28d"
+export ETH_RPC_URL=""
+export PRIVATE_KEY=""
+export ETHERSCAN_API_KEY=""
+export SELLOUT_PROTOCOL_WALLET=""
 
 # Set the address of the proxy you want to upgrade
 export PROXY_ADDRESS="0xe9c1727B668Ce1106FAA49ceA48bf1b33527E913"
