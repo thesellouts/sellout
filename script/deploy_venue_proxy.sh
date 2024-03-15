fbc4a617bd41233808d75b46f3b1f1db70630a14
taayyohh <milesxbridges@gmail.com>
2024-03-14 23:03:40 -0400

    update venue

--- script/deploy_venue_proxy.sh
+++ script/deploy_venue_proxy.sh
@@ -2,12 +2,12 @@
 
 export ETH_RPC_URL=""
 export PRIVATE_KEY=""
-export ETHERSCAN_API_KEY=""
 export SELLOUT_PROTOCOL_WALLET=""
+export ETHERSCAN_API_KEY=""
 
 # Set the address of the proxy you want to upgrade
-export PROXY_ADDRESS="0x2792D5255515bED5C6DdF0c02cB135d8fCB85BaC"
-export NEW_IMPLEMENTATION_ADDRESS="0xD3FE0f84442D5FF303760eb31B583EE89e7C9C13" # The address of the newly deployed implementation contract
+export PROXY_ADDRESS="0xAE8C09948fE8cA98adAB9f560E352Df70C25AcC7"
+export NEW_IMPLEMENTATION_ADDRESS="0x2a002B1a0CbBBb855a5480FF367400a1F0b05648" # The address of the newly deployed implementation contract
 
 # Run the Forge script to upgrade the proxy to the new implementation
 forge script DeployProxy.s.sol:DeployProxy --rpc-url $ETH_RPC_URL --private-key $PRIVATE_KEY --broadcast --verify --etherscan-api-key YOUR_ETHERSCAN_API_KEY
