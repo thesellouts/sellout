bfe4024fc30bac688b26b9a8cd15d0aede073213
taayyohh <milesxbridges@gmail.com>
2024-03-31 21:57:37 -0400

    update contracts

--- script/deploy_show_proxy.sh
+++ script/deploy_show_proxy.sh
@@ -6,8 +6,8 @@ export ETHERSCAN_API_KEY=""
 export SELLOUT_PROTOCOL_WALLET=""
 
 # Set the address of the proxy you want to upgrade
-export PROXY_ADDRESS="0x1672800fF69f5fCE6cb394ff2aC3e7F83F1f44FB"
-export NEW_IMPLEMENTATION_ADDRESS="0xb4f1b6031D6F5161cd64D9ABbc39075237dF1Cf7" # The address of the newly deployed implementation contract
+export PROXY_ADDRESS="0x4F42857dc6e937D5b3131873561785a87AAa3E16"
+export NEW_IMPLEMENTATION_ADDRESS="0x7c0689dB10578A91516eBE4e4731371226D6f516" # The address of the newly deployed implementation contract
 
 # Run the Forge script to upgrade the proxy to the new implementation
 forge script DeployProxy.s.sol:DeployProxy --rpc-url $ETH_RPC_URL --private-key $PRIVATE_KEY --broadcast --verify --etherscan-api-key YOUR_ETHERSCAN_API_KEY
--- script/deploy_ticket_proxy.sh
+++ script/deploy_ticket_proxy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export ETH_RPC_URL=""
-export PRIVATE_KEY=""
-export ETHERSCAN_API_KEY=""
-export SELLOUT_PROTOCOL_WALLET=""
+export ETH_RPC_URL="https://sepolia.infura.io/v3/2WzgmaEOqz7wdfQaPYe2hnAFSKg"
+export PRIVATE_KEY="9ad8ea34cdd8e1f6f4272c7884a62da1c7db612377999eab3d8cf9a653e4c19a"
+export ETHERSCAN_API_KEY="IPUKZ3RRCUJ47Q95JKIBW58Z3EFCDVAMX6"
+export SELLOUT_PROTOCOL_WALLET="0x1dD37D479ac16113fF8f160210Ee209944d2b28d"
 
 # Set the address of the proxy you want to upgrade
-export PROXY_ADDRESS="0x2815ef0140d8e066c0D4773f881B57ca65b80bFF"
-export NEW_IMPLEMENTATION_ADDRESS="0xFa8F52FA81662897e4459FAFF7Ec8931219693ED" # The address of the newly deployed implementation contract
+export PROXY_ADDRESS="0xB7396333135FD3D20e357c29A6685F0ae8aa699a"
+export NEW_IMPLEMENTATION_ADDRESS="0xB0C8e4E34E6EC2A6aAb4E28aa14277BF5c8bDF20" # The address of the newly deployed implementation contract
 
 # Run the Forge script to upgrade the proxy to the new implementation
 forge script DeployProxy.s.sol:DeployProxy --rpc-url $ETH_RPC_URL --private-key $PRIVATE_KEY --broadcast --verify --etherscan-api-key YOUR_ETHERSCAN_API_KEY
