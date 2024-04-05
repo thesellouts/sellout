e20fa398cf4ab588eaf30cd5d170aa66da2de483
taayyohh <milesxbridges@gmail.com>
2024-04-04 20:36:36 -0400

    update abis

--- script/deploy_ticket_proxy.sh
+++ script/deploy_ticket_proxy.sh
@@ -6,8 +6,8 @@ export SELLOUT_PROTOCOL_WALLET=""
 export ETHERSCAN_API_KEY=""
 
 # Set the address of the proxy you want to upgrade
-export PROXY_ADDRESS="0xB7396333135FD3D20e357c29A6685F0ae8aa699a"
-export NEW_IMPLEMENTATION_ADDRESS="0x11E32F00aBA24ecf2Ad590F83e36138f996A55b9" # The address of the newly deployed implementation contract
+export PROXY_ADDRESS="0x11E32F00aBA24ecf2Ad590F83e36138f996A55b9"
+export NEW_IMPLEMENTATION_ADDRESS="0xD8ff53e18598Fb73Ca10443CAe05BA9294CEc823" # The address of the newly deployed implementation contract
 
 # Run the Forge script to upgrade the proxy to the new implementation
 forge script DeployProxy.s.sol:DeployProxy --rpc-url $ETH_RPC_URL --private-key $PRIVATE_KEY --broadcast --verify --etherscan-api-key YOUR_ETHERSCAN_API_KEY
