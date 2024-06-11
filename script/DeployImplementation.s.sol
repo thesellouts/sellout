4ea89f03f81068ec9d52a07cb85a4aee9a61d0ab
taayyohh <milesxbridges@gmail.com>
2024-06-10 21:34:42 -0400

    update indexer

--- script/DeployImplementation.s.sol
+++ script/DeployImplementation.s.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
-import "../src/ticket/Ticket.sol";
+import "../src/venue/Venue.sol";
 
 contract DeployImplementation is Script {
     function run() external {
         vm.startBroadcast();
 
-        Ticket newTicket = new Ticket();
-        console.log("NewTicketImplementation deployed at", address(newTicket));
+        Venue newVenue = new Venue();
+        console.log("NewVenueImplementation deployed at", address(newVenue));
 
         vm.stopBroadcast();
     }
